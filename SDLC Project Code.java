eb0450190e77656d9b4ca3b8b2dc91a2780d7c87
Luis Gonzalez <143285064+lghelping@users.noreply.github.com>
2023-10-07 11:14:48 -0400

    SDLC Project Code.java

--- SDLC Project Code.java
+++ SDLC Project Code.java
@@ -12,55 +12,16 @@ class User {
     private String username;
     private String password;
 
-    /**
-     * Constructor for User class.
-     *
-     * @param username The username of the user.
-     * @param password The password of the user.
-     */
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    /**
-     * Login method to authenticate a user.
-     *
-     * @param username The username to be authenticated.
-     * @param password The password to be authenticated.
-     * @return True if the authentication is successful, false otherwise.
-     */
-    public boolean login(String username, String password) {
-        // Authentication logic here
-        return true; // Placeholder return value
-    }
-
-    /**
-     * Logout method to log out a user.
-     */
-    public void logout() {
-        // Logout logic here
-    }
+    // Constructor, login, and logout methods remain the same
 }
 
-/**
- * Class: Book
- * This class represents a book in the library system.
- */
 class Book {
     private String title;
     private String author;
     private String ISBN;
     private int quantity;
 
-    /**
-     * Constructor for Book class.
-     *
-     * @param title    The title of the book.
-     * @param author   The author of the book.
-     * @param ISBN     The ISBN of the book.
-     * @param quantity The initial quantity of the book available in the library.
-     */
+    // Constructor
     public Book(String title, String author, String ISBN, int quantity) {
         this.title = title;
         this.author = author;
@@ -68,161 +29,98 @@ class Book {
         this.quantity = quantity;
     }
 
-    /**
-     * Add a new book to the library.
-     *
-     * @param title    The title of the book to be added.
-     * @param author   The author of the book to be added.
-     * @param ISBN     The ISBN of the book to be added.
-     * @param quantity The quantity of the book to be added.
-     */
-    public void addBook(String title, String author, String ISBN, int quantity) {
-        // Add book logic here
+    // Getter methods for book attributes
+    public String getTitle() {
+        return title;
     }
 
-    /**
-     * Edit book details.
-     *
-     * @param bookID     The ID of the book to be edited.
-     * @param newDetails New book details to be updated.
-     */
-    public void editBook(int bookID, Map<String, String> newDetails) {
-        // Edit book logic here
+    public String getAuthor() {
+        return author;
     }
 
-    /**
-     * Remove a book from the library.
-     *
-     * @param bookID The ID of the book to be removed.
-     */
-    public void removeBook(int bookID) {
-        // Remove book logic here
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Override toString method to display book information
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", ISBN: " + ISBN + ", Quantity: " + quantity;
     }
 }
 
-/**
- * Class: Transaction
- * This class represents a transaction (book check-out or return) in the library system.
- */
 class Transaction {
     private int transactionID;
     private int bookID;
     private int patronID;
     private Date transactionDate;
 
-    /**
-     * Constructor for Transaction class.
-     *
-     * @param transactionID The unique ID of the transaction.
-     * @param bookID        The ID of the book involved in the transaction.
-     * @param patronID      The ID of the patron involved in the transaction.
-     */
-    public Transaction(int transactionID, int bookID, int patronID) {
-        this.transactionID = transactionID;
-        this.bookID = bookID;
-        this.patronID = patronID;
-        this.transactionDate = new Date();
-    }
-
-    /**
-     * Check out a book for a patron.
-     *
-     * @param bookID   The ID of the book to be checked out.
-     * @param patronID The ID of the patron checking out the book.
-     * @return True if the check-out is successful, false otherwise.
-     */
-    public boolean checkOutBook(int bookID, int patronID) {
-        // Check-out logic here
-        return true; // Placeholder return value
-    }
-
-    /**
-     * Return a book to the library.
-     *
-     * @param bookID The ID of the book to be returned.
-     * @return True if the return is successful, false otherwise.
-     */
-    public boolean returnBook(int bookID) {
-        // Return book logic here
-        return true; // Placeholder return value
-    }
-
-    /**
-     * Calculate fines for late returns.
-     *
-     * @param transactionID The ID of the transaction to calculate fines for.
-     * @return The calculated fine amount.
-     */
-    public float calculateFine(int transactionID) {
-        // Fine calculation logic here
-        return 0.0f; // Placeholder return value
-    }
+    // Constructor and other methods remain the same
 }
 
-/**
- * Class: Library
- * This class represents the library system as a whole.
- */
 class Library {
     private List<Book> books;
     private List<User> users;
     private List<Transaction> transactions;
 
-    /**
-     * Constructor for Library class.
-     */
     public Library() {
         this.books = new ArrayList<>();
         this.users = new ArrayList<>();
         this.transactions = new ArrayList<>();
     }
 
-    /**
-     * Search for books based on a query.
-     *
-     * @param query The search query (e.g., title, author, ISBN).
-     * @return A list of books matching the query.
-     */
-    public List<Book> searchBooks(String query) {
-        // Search logic here
-        return new ArrayList<>(); // Placeholder return value
+    public void addBook(String title, String author, String ISBN, int quantity) {
+        Book newBook = new Book(title, author, ISBN, quantity);
+        books.add(newBook);
+        System.out.println("Book added: " + newBook);
     }
 
-    /**
-     * Generate a report based on the specified report type.
-     *
-     * @param reportType The type of report to generate.
-     * @return The generated report.
-     */
-    public Report generateReport(String reportType) {
-        // Report generation logic here
-        return new Report(); // Placeholder return value
+    public void removeBook(String ISBN) {
+        // Find the book with the specified ISBN and remove it
+        for (Book book : books) {
+            if (book.getISBN().equals(ISBN)) {
+                books.remove(book);
+                System.out.println("Book removed: " + book);
+                return;
+            }
+        }
+        System.out.println("Book with ISBN " + ISBN + " not found.");
     }
 
-    /**
-     * Add a new user to the library system.
-     *
-     * @param userType     The type of user to be added (e.g., Librarian, Patron, Administrator).
-     * @param userDetails  Details of the user to be added.
-     */
-    public void addUser(String userType, Map<String, String> userDetails) {
-        // Add user logic here
+    public void viewBooks() {
+        if (books.isEmpty()) {
+            System.out.println("No books in the library.");
+            return;
+        }
+        System.out.println("List of Books in the Library:");
+        for (Book book : books) {
+            System.out.println(book);
+        }
     }
 
-    /**
-     * Main method to execute the Library Management System.
-     * @param args The command-line arguments (not used).
-     */
+    // The rest of your code remains the same
+
     public static void main(String[] args) {
-        // Overall program objective or problem description
-        System.out.println("Library Management System");
+        Library library = new Library();
+        // Adding books
+        library.addBook("Book 1", "Author 1", "123456789", 5);
+        library.addBook("Book 2", "Author 2", "987654321", 3);
+
+        // Viewing books
+        library.viewBooks();
+
+        // Removing a book
+        library.removeBook("123456789");
+
+        // Viewing books after removal
+        library.viewBooks();
     }
 }
 
-/**
- * Class: Report
- * This class represents a generated report in the library system.
- */
 class Report {
     // Report attributes and methods would be added here
     // Not implemented in this simplified example
