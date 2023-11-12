13b8292dfe37344b78a6aa89476f7e6194920aba
Luis Gonzalez <143285064+lghelping@users.noreply.github.com>
2023-11-12 10:03:26 -0500

    GUI LMS Project Phase 3
    Here is the Graphical user interface ( GUI)

---
+++ package_info.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ * The GUI package contains classes related to the graphical user interface of the library management system.
+ */
+package GUI;
+
+/**
+ *
+ * @author Mudassiri
+ */
+
