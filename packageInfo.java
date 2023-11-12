f7e3e3cdb3bfaaf93d72d123ba00ab2f20f49aba
Luis Gonzalez <143285064+lghelping@users.noreply.github.com>
2023-11-12 10:08:32 -0500

    Delete packageInfo.java

--- packageInfo.java
+++
@@ -1,5 +0,0 @@
-/**
- * The library_management_system package contains classes for the library management system.
- * This package includes functionality for database connection and GUI.
- */
-package library_managament_system;
