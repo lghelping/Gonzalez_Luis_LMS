57bf80baf7ccba06489459406d5d26da9207a2e6
Luis Gonzalez <143285064+lghelping@users.noreply.github.com>
2023-11-12 10:10:50 -0500

    LMS Project pt3
    Here, you can find the codes to integrate your Database with your LMS Project

---
+++ packageInfo.java
@@ -0,0 +1,5 @@
+/**
+ * The library_management_system package contains classes for the library management system.
+ * This package includes functionality for database connection and GUI.
+ */
+package library_managament_system;
